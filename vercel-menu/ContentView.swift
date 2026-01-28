8eb84f8b32a1124283cd07394fc7b0f5545a989a
Ryan <ryanjmarcus@gmail.com>
2026-01-28 16:13:13 -0500

    Initial Commit

---
+++ vercel-menu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vercel-menu
+//
+//  Created by Ryan Marcus on 1/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vercel-menu/vercel_menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  vercel_menuApp.swift
+//  vercel-menu
+//
+//  Created by Ryan Marcus on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct vercel_menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
